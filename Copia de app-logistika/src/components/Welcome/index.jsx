63cbcf76d9bfcb508506bb4ac2b979743495f774
DealoArroyo <132007917+DealoArroyo@users.noreply.github.com>
2024-10-03 13:01:14 -0600

    Inicio de aplicación web
    Se inicializó la aplicación. Añadiendo solamente el NavBar y una parte de texto. Esta primera parte ya es responsive con el método Mobile First.

---
+++ Copia de app-logistika/src/components/Welcome/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Welcome = () => {
+  return (
+    <div className='p-5'>
+        <h2 className='text-center font-bold'>¿Quieres un evento espectacular en uno de los lugares más bonitos y espaciosos de Querétaro donde la clase y el buen gusto no te cuesta más?</h2>
+    </div>
+  )
+}
+
+export default Welcome
